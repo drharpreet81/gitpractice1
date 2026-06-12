518821ee0b63e91f0f5588c9072c4084d4a40dd6
hk123 <drhk2005@gmail.com>
2026-06-12 12:18:32 +0530

    modified file

--- employee.py
+++ employee.py
@@ -1,6 +1,6 @@
 print("Employee Details")
-print("Employee ID: E101")
+print("Employee ID: E102")
 print("Name: John Smith")
 print("Department: IT")
-print("Salary: 50000")
+print("Salary: 50001")
 
